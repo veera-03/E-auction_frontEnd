f85d52d2cdfee941be08b9618571d8086a571a34
VEERAKUMAR T <veerakumarthangavel4@gmail.com>
2024-08-19 09:51:47 +0530

    bidding history

--- src/components/Bidded_details.jsx
+++ src/components/Bidded_details.jsx
@@ -6,8 +6,8 @@ const [bidded_history, setbidded_history] = useState('cvgjgjvj')
 
     axios.get(`https://e-auction.onrender.com/bikebidded_details`)
     .then((result) => {
-        console.log(result.data)
-        setbidded_history(result.data)
+        console.log(result.data[0].email)
+        setbidded_history(result.data[0].email)
     })
     .catch((error) => {
       console.error("Error fetching amount:", error);
